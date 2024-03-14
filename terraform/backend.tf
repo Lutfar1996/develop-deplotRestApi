16f1e59a75a829bfe9d34ffed7107ea057f0709b
lutfar1996 <lutfar.rahman569@gmail.com>
2024-03-14 14:32:46 +0600

    Change region

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
     key            = "kubernetes-cluster/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-west-2"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
